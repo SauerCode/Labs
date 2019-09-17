79de258852beeda84fed110268a2422beedb3902
Alexandru Sauer <robertasauer@yahoo.com>
2019-09-17 17:57:46 -0400

    Change

--- Lab1_Git/PersonInfo4.java
+++ Lab1_Git/PersonInfo4.java
@@ -3,7 +3,7 @@ public class PersonInfo4 {
     public static String firstName = "Alex";
     public static String lastName = "Moujot";
     public static int studentNo = 88499;
-    public static String description = "les ananas";
+    public static String description = "les anasanas";
     
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
