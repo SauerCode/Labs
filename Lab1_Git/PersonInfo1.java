503256c70669bb0477e317f5aef13869afd252b4
Alexandru Sauer <robertasauer@yahoo.com>
2019-09-17 17:43:54 -0400

    Modify Person1

--- Lab1_Git/PersonInfo1.java
+++ Lab1_Git/PersonInfo1.java
@@ -1,8 +1,8 @@
 public class PersonInfo1 {
 
-    public static String firstName = "Leisha";
-    public static String lastName = "Baker";
-    public static int studentNo = 338299;
+    public static String firstName = "Alexandru";
+    public static String lastName = "Sauer";
+    public static int studentNo = 300026641;
     public static String description = "Likes to take pictures";
 
     public static void displayFr() {
